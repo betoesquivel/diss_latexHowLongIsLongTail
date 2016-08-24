fae38478c76964af4ea04b67b1b3d1d839a2161d
Beto Esquivel <ppbeto94@gmail.com>
2016-08-24 11:23:07 +0100

    adds query for caching a table

---
+++ queries/cache_table.sql
@@ -0,0 +1,10 @@
+\begin{lstlisting}[
+           language=SQL,
+           showspaces=false,
+           basicstyle=\ttfamily,
+           numbers=left,
+           numberstyle=\tiny,
+           commentstyle=\color{gray}
+        ]
+cache table tags_s3
+\end{lstlisting}
